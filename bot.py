be382741aed306df7414c37db974e22775b7c278
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2021-06-26 18:05:13 +0530

    Update bot.py
    added only typing forever

--- bot.py
+++ bot.py
@@ -13,18 +13,18 @@ async def on_message(message):
            return
       if message.channel.id==798556779892965376 or message.channel.id==855841358735867904:
           async with message.channel.typing():
-              try:
-                  msg = message.content
-                  if message.mentions != []:
-                      for i in message.mentions:
-                          msg = msg.replace(f'<@!{i.id}>', i.name)
+              #try:
+                  ######msg = message.content
+                 ##### if message.mentions != []:
+                   ####   for i in message.mentions:
+                  ###        msg = msg.replace(f'<@!{i.id}>', i.name)
 
-                  response = await rs.get_ai_response(msg)
-              except json.decoder.JSONDecodeError:
-                  await asyncio.sleep(3)
-                  response = await rs.get_ai_response(msg)
+                ##  response = await rs.get_ai_response(msg)
+           #   except json.decoder.JSONDecodeError:
+                #  await asyncio.sleep(3)
+               #   response = await rs.get_ai_response(msg)
 
-          await message.reply("**"+response[0]["message"]+"**")
+         # await message.reply("**"+response[0]["message"]+"**")
 TOKEN ="NzgxOTAyMjc1NDcxNDc0NzE5.X8EZPQ.EZ1W2nAIG7TAK9Ch-vc6C3Y7vqs"
 bot.run(TOKEN)
 # while True:
